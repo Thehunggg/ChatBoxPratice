63422a3e4172aec95f0e153c1a9fc4e5ebfd47cd
ThehungIT11A <hdothe1@gmail.com>
2026-07-27 10:28:22 +0900

    Expressで本番用フロントを配信

--- messenger-backend/server.js
+++ messenger-backend/server.js
@@ -19,6 +19,7 @@ require("dotenv").config();
 const express = require("express");
 
 const app = express();
+app.use(express.static("../frontend/dist"));
 app.use(express.json());
 app.use("/images", express.static("public/images"));
 
